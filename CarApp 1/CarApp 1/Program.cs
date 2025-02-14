def561be550d3ca2db07a2ac3f01a052c02f286d
99911089 <158296125+99911089@users.noreply.github.com>
2025-02-14 12:37:31 +0100

    Add files via upload

---
+++ CarApp 1/CarApp 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Brugeren indtaster biloplysninger
+        Console.WriteLine("Indtast bilmærke:");
+        string bilModel = Console.ReadLine();
+
+        Console.WriteLine("Indtast bilens årgang:");
+        int bilÅrgang;
+
+        while (!int.TryParse(Console.ReadLine(), out bilÅrgang))
+        {
+            Console.WriteLine("Fejl! Indtast venligst et gyldigt årstal:");
+        }
+
+        Console.WriteLine("Indtast brændstoftype:");
+        string brændstofBeskrivelse = Console.ReadLine();
+
+        // Formatér output med PadLeft og PadRight
+        Console.WriteLine();
+        Console.WriteLine("Biloplysninger:");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Type bil".PadRight(20) + "Info om bil".PadLeft(20));
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Bilmærke:".PadRight(20) + bilModel.PadLeft(20));
+        Console.WriteLine("Årgang:".PadRight(20) + bilÅrgang.ToString().PadLeft(20));
+        Console.WriteLine("Brændstoftype:".PadRight(20) + brændstofBeskrivelse.PadLeft(20));
+        Console.ReadLine();
+    }
+}
+
+

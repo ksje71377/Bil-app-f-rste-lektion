a153b7b34473d6bdda6f181048edec99d1e2bb7f
Michael Kragh <mikr71394@edu.ucl.dk>
2025-02-16 14:51:20 +0100

    mike comments

--- Bil-app, første lektion/Program.cs
+++ Bil-app, første lektion/Program.cs
@@ -6,6 +6,13 @@ namespace Bil_app__første_lektion
     {
         static void Main(string[] args)
         {
+            /*
+             * Dette er Team14's biloplysnings projekt med beregninger om tur og brændstof.
+             * Lavet af UCL Studerende på Online Datamatiker uddannelsen. Dato: Feb 2025.
+             * Copyright (c) 2025 Team14 - DMOoF25 UCL Erhvervsakademi.
+             */
+            
+            //Alle variablerne i eksplicit form.
             string bilModel;
             int bilÅrgang;
             char bilGearkasse;
@@ -19,31 +26,71 @@ namespace Bil_app__første_lektion
             int kilometerstandEfter;
             string gearBeskrivelse;
             string brændstofBeskrivelse;
+            string bilFabrikant; 
+            
+            //Velkomst Intro og ny linje.
             Console.WriteLine("Velkommen til CarApp version 1.0\n");
+            
+            //Spørgsmål: Bil-Fabrikant med feedback fra brugeren.
             Console.WriteLine("Hvilken fabrikant har lavet din bil?:");
-            string bilFabrikant = Console.ReadLine();
+            bilFabrikant = Console.ReadLine();
+            
+            //Spørgsmål: Bil-Model med feedback fra brugeren.
             Console.WriteLine("Hvilken model er din bil?");
             bilModel = Console.ReadLine();
+            
+            //Spørgsmål: Bil-Model med feedback fra brugeren konvertreret til int.
             Console.WriteLine("Hvor mange kilometer har bilen kørt?:");
             bilKilometerstand = Convert.ToInt32(Console.ReadLine());
+            
+            //Spørgsmål: Bil-Brændstoftype med feedback fra brugeren.
             Console.WriteLine("Hvilket slags brændstof bruger den? B for benzin, D for diesel");
             bilBrændstof = Console.ReadLine()[0];
+            
+            //Spørgsmål: Bil-Forbrug med feedback fra brugeren konverteret til double.
             Console.WriteLine("Hvor langt kører den på literen? (Angiv i km/l.):");
             bilForbrug = Convert.ToDouble(Console.ReadLine());
+            
+            //Spørgsmål: Bil-årgang med feedback fra brugeren konverteret til int.
             Console.WriteLine("Hvad årgang er din bil?");
             bilÅrgang = Convert.ToInt32(Console.ReadLine());
+            
+            //Spørgsmål: Bil-Gear type med feedback fra brugeren og kun første bogstav hentes.
             Console.WriteLine("Er bilens gear manuelt eller automatisk? Tast A for auto, M for manuel.");
             bilGearkasse = Console.ReadLine()[0];
+            
+            /*
+             * Bestemmer beskrivelse af gearkasse og brændstoftype baseret på brugerens input.
+             * - Hvis brugeren taster 'A', sættes gearBeskrivelse til "automatgear".
+             * - Hvis brugeren taster 'M', sættes gearBeskrivelse til "manuelt gear".
+             * - Hvis inputtet er noget andet, sættes gearBeskrivelse til "ukendt, du har tastet forkert input".
+             *
+             * På samme måde bestemmes brændstofBeskrivelse baseret på brugerens input:
+             * - 'B' giver "benzin".
+             * - 'D' giver "diesel".
+             * - Andet input returnerer "ukendt brændstoftype".
+             */
             gearBeskrivelse = bilGearkasse == 'A' ? "automatgear" : (bilGearkasse == 'M' ? "manuelt gear" : "ukendt, du har tastet forkert input");
             brændstofBeskrivelse = bilBrændstof == 'B' ? "benzin" : (bilBrændstof == 'D' ? "diesel" : "ukendt brændstoftype, du har tastet forkert input");
+            
+            //Oplysninger til brugeren, om bilen ud fra det der er blevet indtastet indtil nu.
             Console.WriteLine("Tak for det indtastede! Vi ved nu at din bil er produceret af " + bilFabrikant + " og er af modellen " + bilModel + " - den er fra år " + bilÅrgang + " og gearkassen er " + gearBeskrivelse + ". Den anvender " + brændstofBeskrivelse + " som drivmiddel, kører " + bilForbrug + " pr. liter " + brændstofBeskrivelse + " og har tilbagelagt i alt " + bilKilometerstand + " kilometer siden den var ny.\n\nDer skal nu beregnes på økonomien i en tur.");
+            
+            //Spørgsmål: Brændstofsøkonomi og input-feedback fra brugeren konverteret til double
             Console.WriteLine("\n\nIndtast venligst gældende pris for en liter brændstof:");
             brændstofPris = Convert.ToDouble(Console.ReadLine());
+            
+            //Spørgsmål: Bil-Tur og input-feedback fra brugeren konverteret til int
             Console.WriteLine("Angiv, hvor lang en tur du skal ud og køre - brug ikke decimaler:");
             turDistance = Convert.ToInt32(Console.ReadLine());
+            
+            //Bilberegninger for Brændstof og Pris samt opdateret kilometertal efter biltur
             brændstofNødvendigt = turDistance / bilForbrug;
             pengeNødvendigt = brændstofNødvendigt * brændstofPris;
             kilometerstandEfter = Convert.ToInt32(bilKilometerstand + turDistance);
+            
+            //Oplysninger til brugeren, om bilen ud fra det der er blevet indtastet indtil nu.
+            //Det er mere præcist at sige, at Console.WriteLine() har samme formatfunktionalitet som string.Format(), hvilket betyder, at vi ikke behøver at kalde string.Format() manuelt.
             Console.WriteLine("\nTil den pågældende tur behøver du {0} liter brændstof\n\t- det vil koste dig {1} kroner i brændstofudgifter!", brændstofNødvendigt, pengeNødvendigt);
             Console.WriteLine("\nFør turen bliver kørt har din bil tilbagelagt {0} kilometer. Efter køreturen vil den have tilbagelagt {1} kilometer!", bilKilometerstand, kilometerstandEfter);
             Console.ReadLine();
